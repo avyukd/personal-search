96b2a2454cf90061fc169a201bb6a8b29202b94c
avyukd <avyukd@gmail.com>
2021-07-27 14:06:17 -0400

    indexer added

---
+++ indexer.py
@@ -0,0 +1,37 @@
+from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
+from whoosh.analysis import StemmingAnalyzer
+from whoosh import index
+import os, os.path
+import textract
+
+TEST_PATH = r"C:\Users\avyuk\OneDrive\Desktop\EeS"
+
+schema = Schema(filename=ID(stored=True),
+                filepath=ID(stored=True),
+                content=TEXT(analyzer=StemmingAnalyzer()),
+                tags=KEYWORD(commas=True, scorable=True))
+
+sample_files = os.listdir(TEST_PATH)
+
+if not os.path.exists("indexdir"):
+    os.mkdir("indexdir")
+    ix = index.create_in("indexdir", schema)
+
+ix = index.open_dir("indexdir")
+
+writer = ix.writer()
+
+for file in sample_files:
+    if ".pdf" in file or ".docx" in file or ".doc" in file:
+        try:
+            filepath = os.path.join(TEST_PATH, file)
+
+            text = textract.process(filepath).decode("utf-8")
+            writer.add_document(filename=file,
+                                filepath=filepath,
+                                content=text)
+        except UnicodeDecodeError:
+            print("UnicodeDecodeError. Moving to next file.")
+writer.commit()
+
+
