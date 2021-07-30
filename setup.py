d3923a220b3d20a16dfc265b980aae2677903a31
avyukd <avyukd@gmail.com>
2021-07-30 09:33:49 -0400

    updated gitignore, working basic auth

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
-# Elasticsearch 7.x
-elasticsearch>=7.0.0,<8.0.0
+from elasticsearch import Elasticsearch, helpers
+import configparser
+
+config = configparser.ConfigParser()
+config.read('example.ini')
+
+es = Elasticsearch(
+    cloud_id=config['ELASTIC']['cloud_id'],
+    http_auth=(config['ELASTIC']['user'], config['ELASTIC']['password'])
+)
+
+print(es.info())
