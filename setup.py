b341338c945637b95b084c1cfb852ec5f3dfff64
avyukd <avyukd@gmail.com>
2021-07-30 09:41:22 -0400

    Working with API auth.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ config = configparser.ConfigParser()
 config.read('example.ini')
 
 es = Elasticsearch(
-    cloud_id=config['ELASTIC']['cloud_id'],
-    http_auth=(config['ELASTIC']['user'], config['ELASTIC']['password'])
+    cloud_id=config['DEFAULT']['cloud_id'],
+    api_key=(config['DEFAULT']['apikey_id'], config['DEFAULT']['apikey_key']),
 )
 
-print(es.indices.delete(index='lord-of-the-rings'))
+print(es.info())
